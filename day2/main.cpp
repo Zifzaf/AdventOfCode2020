b624ca98509b17fbb5b4b20711f47553a270a1b3
Andreas Kreuzmann <andreas.kreuzmann@sensirion.com>
2020-12-03 07:03:26 +0100

    Restructure repo

--- day2/main.cpp
+++ day2/main.cpp
@@ -4,49 +4,7 @@
 #include <vector>
 using namespace std;
 
-int day1level1(ifstream &file) {
-  int i = 0, j = 0;
-  vector<int> list;
-  while (true) {
-    int in;
-    file >> in;
-    i = list.size();
-    for (j = 0; j < i; j++) {
-      if (in + list[j] == 2020) {
-        return in * list[j];
-      }
-    }
-    list.push_back(in);
-  }
-}
-
-int day1level2(ifstream &file) {
-  int i = 0, j = 0;
-  bool ind[2020];
-  vector<int> list;
-  int nums[2020][2];
-  while (true) {
-    int in;
-    file >> in;
-    if (ind[2020 - in]) {
-      cout << in << " " << nums[2020 - in][0] << " " << nums[2020 - in][1]
-           << endl;
-      return in * nums[2020 - in][0] * nums[2020 - in][1];
-    }
-    i = list.size();
-    for (j = 0; j < i; j++) {
-      int sum = in + list[j];
-      if (sum < 2020 && !ind[sum]) {
-        ind[sum] = true;
-        nums[sum][0] = in;
-        nums[sum][1] = list[j];
-      }
-    }
-    list.push_back(in);
-  }
-}
-
-int day2level1(ifstream &file) {
+int level1(ifstream &file) {
   int min, max, count, out = 0;
   char letter, next, s;
   while (file.peek() != EOF) {
@@ -72,7 +30,7 @@ int day2level1(ifstream &file) {
   return out;
 }
 
-int day2level2(ifstream &file) {
+int level2(ifstream &file) {
   bool check;
   int one, two, out = 0, t;
   char letter, next, s;
@@ -104,6 +62,6 @@ int day2level2(ifstream &file) {
 int main() {
   ifstream file;
   file.open("input");
-  int out = day2level2(file);
+  int out = level2(file);
   cout << endl << "Result: " << out << endl;
 }
